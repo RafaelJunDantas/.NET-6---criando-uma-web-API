c0664b6e8fd101a369998232b6b4f13c7376e482
Rafael <rafajdantas@gmail.com>
2024-06-24 15:02:03 -0300

    adjust file structure

--- FilmesAPI/Migrations/20240624171605_CriandoTabelaDeFilme.cs
+++ FilmesAPI/Migrations/20240624171605_CriandoTabelaDeFilme.cs
@@ -3,38 +3,36 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
-namespace FilmesAPI.Migrations
+namespace FilmesAPI.Migrations;
+public partial class CriandoTabelaDeFilme : Migration
 {
-    public partial class CriandoTabelaDeFilme : Migration
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AlterDatabase()
-                .Annotation("MySql:CharSet", "utf8mb4");
+        migrationBuilder.AlterDatabase()
+            .Annotation("MySql:CharSet", "utf8mb4");
 
-            migrationBuilder.CreateTable(
-                name: "Filmes",
-                columns: table => new
-                {
-                    id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
-                    titulo = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
-                        .Annotation("MySql:CharSet", "utf8mb4"),
-                    genero = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
-                        .Annotation("MySql:CharSet", "utf8mb4"),
-                    duracao = table.Column<int>(type: "int", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Filmes", x => x.id);
-                })
-                .Annotation("MySql:CharSet", "utf8mb4");
-        }
+        migrationBuilder.CreateTable(
+            name: "Filmes",
+            columns: table => new
+            {
+                id = table.Column<int>(type: "int", nullable: false)
+                    .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                titulo = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                genero = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                    .Annotation("MySql:CharSet", "utf8mb4"),
+                duracao = table.Column<int>(type: "int", nullable: false)
+            },
+            constraints: table =>
+            {
+                table.PrimaryKey("PK_Filmes", x => x.id);
+            })
+            .Annotation("MySql:CharSet", "utf8mb4");
+    }
 
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Filmes");
-        }
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.DropTable(
+            name: "Filmes");
     }
 }
