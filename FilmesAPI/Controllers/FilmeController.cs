0251f1d4e63752e5b5d8033c8a4708236dc0e068
Rafael <rafajdantas@gmail.com>
2024-06-24 15:00:44 -0300

    get filmes from database

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesAPI.Models;
+﻿using FilmesAPI.Data;
+using FilmesAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
@@ -8,8 +9,12 @@ namespace FilmesAPI.Controllers;
 
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> filmes = new List<Filme>();
-    private static int id = 0;
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     private void PrintFilme(Filme filme)
     {
@@ -23,41 +28,42 @@ public class FilmeController : ControllerBase
     [HttpPost]
     public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
-        filme.id = id++;
-        filmes.Add(filme);
-        PrintFilme(filme);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        //PrintFilme(filme);
         return CreatedAtAction(nameof(GetFilme), new { id = filme.id }, filme);
     }
 
     [HttpGet]
     public IActionResult GetFilmes([FromQuery] int take = 0, [FromQuery] int skip = 0)
     {
-        if (filmes.Count == 0)
+
+        if (_context.Filmes.Count() == 0)
         {
-            Console.WriteLine("Nao ha filmes cadastrados!");
-            return Ok(filmes);
+            //Console.WriteLine("Nao ha filmes cadastrados!");
+            return Ok((Microsoft.EntityFrameworkCore.DbSet<Filme>?)_context.Filmes);
         }
-        if(take == 0) take = filmes.Count; 
-        Console.WriteLine("Filmes Cadastrados:");
-        foreach (Filme filme in filmes.Skip(skip).Take(take))
-        {
-            PrintFilme(filme);
-        }
-        return Ok(filmes.Skip(skip).Take(take));
+        if (take == 0) take = _context.Filmes.Count();
+        //Console.WriteLine("Filmes Cadastrados:");
+        //foreach (Filme filme in _context.Filmes.Skip(skip).Take(take))
+        //{
+        //    PrintFilme(filme);
+        //}
+        return Ok(_context.Filmes.Skip(skip).Take(take));
     }
 
     [HttpGet("{id}")]
     public IActionResult GetFilme(int id)
     {
-        var filme = filmes.FirstOrDefault(filme => filme.id == id);
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.id == id);
 
         if (filme == null)
         {
-            Console.WriteLine($"Nao existe filme com o id {id}");
+            //Console.WriteLine($"Nao existe filme com o id {id}");
             return NotFound();
         }
 
-        PrintFilme(filme);
+        //PrintFilme(filme);
         return Ok(filme);
     }
 }
