4615528bcd68819c941d04752c53552da0465c2f
Rafael <rafajdantas@gmail.com>
2024-06-24 16:18:49 -0300

    update swagger docs

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -30,7 +30,14 @@ public class FilmeController : ControllerBase
         Console.WriteLine($"Duracao do filme: {filme.duracao}");
     }
 
+    /// <summary>
+    /// Adiciona um filme ao banco de dados
+    /// </summary>
+    /// <param name="filmeDTO">Objeto com os atributos necessários para a criaçãode um filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="201">Caso a criação e inserção sejam sucesso</response>
     [HttpPost]
+    [ProducesResponseType( StatusCodes.Status201Created)]
     public IActionResult AdicionaFilme([FromBody] CreateFilmeDTO filmeDTO)
     {
         Filme filme = _mapper.Map<Filme>(filmeDTO);
@@ -40,7 +47,15 @@ public class FilmeController : ControllerBase
         return CreatedAtAction(nameof(GetFilme), new { id = filme.id }, filme);
     }
 
+    /// <summary>
+    /// Retorna uma quantidade ou todos os filmes do banco de dados
+    /// </summary>
+    /// <param name="take">Quantos filmes quer que retorno, se não especificar, retorna todos</param>
+    /// <param name="skip">Quantos filmes quer pular</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso a requisiçao seja sucesso</response>
     [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult GetFilmes([FromQuery] int take = 0, [FromQuery] int skip = 0)
     {
 
@@ -58,7 +73,14 @@ public class FilmeController : ControllerBase
         return Ok(_mapper.Map<List<ReadFilmeDTO>>(_context.Filmes.Skip(skip).Take(take)));
     }
 
+    /// <summary>
+    /// Retorna o filmes especificado pelo ID
+    /// </summary>
+    /// <param name="id">ID do filme no banco de dados</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso o filme com o ID exista no banco de dados</response>
     [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
     public IActionResult GetFilme(int id)
     {
         var filme = _context.Filmes.FirstOrDefault(filme => filme.id == id);
@@ -75,7 +97,15 @@ public class FilmeController : ControllerBase
         return Ok(filmeDTO);
     }
 
+    /// <summary>
+    /// Atualiza um filme por ID
+    /// </summary>
+    /// <param name="id">ID do filme para atualizar</param>
+    /// <param name="filmeDTO">Objeto com os atributos para atualizar</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso consiga atualizar o filme com o ID especificado</response>
     [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult UpdateFilme(int id, [FromBody] UpdateFilmeDTO filmeDTO)
     {
         var filme = _context.Filmes.FirstOrDefault(filme => filme.id == id);
@@ -88,7 +118,15 @@ public class FilmeController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Atualiza um atibuto do filme por ID
+    /// </summary>
+    /// <param name="id">ID do filme para atualizar</param>
+    /// <param name="patch">Objeto com os atributos para atualizar</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso consiga atualizar o filme com o ID especificado</response>
     [HttpPatch("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult PatchFilme(int id, JsonPatchDocument<UpdateFilmeDTO> patch)
     {
         var filme = _context.Filmes.FirstOrDefault(filme => filme.id == id);
@@ -110,7 +148,14 @@ public class FilmeController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Delete um filme por ID
+    /// </summary>
+    /// <param name="id">ID do filme para deletar</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso o filme seja deletado</response>
     [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
     public IActionResult DeleteFilme(int id)
     {
         var filme = _context.Filmes.FirstOrDefault(filme => filme.id == id);
