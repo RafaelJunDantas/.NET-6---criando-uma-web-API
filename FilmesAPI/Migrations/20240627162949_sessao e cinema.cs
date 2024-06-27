e60e25f553717fcb38922cbdc0a608b450eac136
Rafael <rafajdantas@gmail.com>
2024-06-27 13:56:58 -0300

    new migrations

---
+++ FilmesAPI/Migrations/20240627162949_sessao e cinema.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesAPI.Migrations
+{
+    public partial class sessaoecinema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "cinemaID",
+                table: "Sessoes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessoes_cinemaID",
+                table: "Sessoes",
+                column: "cinemaID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sessoes_Cinemas_cinemaID",
+                table: "Sessoes",
+                column: "cinemaID",
+                principalTable: "Cinemas",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sessoes_Cinemas_cinemaID",
+                table: "Sessoes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Sessoes_cinemaID",
+                table: "Sessoes");
+
+            migrationBuilder.DropColumn(
+                name: "cinemaID",
+                table: "Sessoes");
+        }
+    }
+}
---
+++ FilmesAPI/Migrations/20240627165533_fix errors.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesAPI.Migrations
+{
+    public partial class fixerrors : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sessoes_Cinemas_cinemaID",
+                table: "Sessoes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "cinemaID",
+                table: "Sessoes",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sessoes_Cinemas_cinemaID",
+                table: "Sessoes",
+                column: "cinemaID",
+                principalTable: "Cinemas",
+                principalColumn: "id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Sessoes_Cinemas_cinemaID",
+                table: "Sessoes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "cinemaID",
+                table: "Sessoes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Sessoes_Cinemas_cinemaID",
+                table: "Sessoes",
+                column: "cinemaID",
+                principalTable: "Cinemas",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
