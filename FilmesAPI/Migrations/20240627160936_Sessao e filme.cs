3d3379104d8f2f183f20c17195016944e22c2507
Rafael <rafajdantas@gmail.com>
2024-06-27 13:22:32 -0300

    new migrations

---
+++ FilmesAPI/Migrations/20240627160936_Sessao e filme.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesAPI.Migrations
+{
+    public partial class Sessaoefilme : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sessoes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    filmeID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessoes", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Sessoes_Filmes_filmeID",
+                        column: x => x.filmeID,
+                        principalTable: "Filmes",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessoes_filmeID",
+                table: "Sessoes",
+                column: "filmeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sessoes");
+        }
+    }
+}
