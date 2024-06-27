12d35ffeff4d6969626cfa9ade5c709c7166486f
Rafael <rafajdantas@gmail.com>
2024-06-27 09:11:20 -0300

    rename FilmeContext to Context

---
+++ FilmesAPI/Data/Context.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesAPI.Data;
+
+public class Context : DbContext
+{
+    public Context(DbContextOptions<Context> opts) : base(opts) 
+    {
+
+    }
+
+    public DbSet<Filme> Filmes { get; set; }
+    public DbSet<Cinema> Cinemas { get; set; }
+    public DbSet<Endereco> Enderecos { get; set; }
+}
+
