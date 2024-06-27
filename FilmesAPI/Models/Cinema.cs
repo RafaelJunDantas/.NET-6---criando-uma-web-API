23af9dbbe40077b7ea15dfb12281f60016b38e90
Rafael <rafajdantas@gmail.com>
2024-06-27 09:15:02 -0300

    ad realtion between cinema and endereco

--- FilmesAPI/Models/Cinema.cs
+++ FilmesAPI/Models/Cinema.cs
@@ -11,4 +11,7 @@ public class Cinema
     [Required(ErrorMessage = "O nome do cinema e obrigatorio")]
     [MaxLength(50, ErrorMessage = "Tamanho do nome do cinema nao pode exceder 50 caracteres")]
     public string nome { get; set; }
+
+    public int enderecoID { get; set; }
+    public virtual Endereco endereco { get; set; }
 }
