8b772fe63bb08cc47b68a240bf405d0944a75b92
Rafael <rafajdantas@gmail.com>
2024-06-28 07:56:34 -0300

    new migrations

---
+++ FilmesAPI/Migrations/20240628105510_delete restrict.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FilmesAPI.Migrations
+{
+    public partial class deleterestrict : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cinemas_Enderecos_enderecoID",
+                table: "Cinemas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cinemas_Enderecos_enderecoID",
+                table: "Cinemas",
+                column: "enderecoID",
+                principalTable: "Enderecos",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cinemas_Enderecos_enderecoID",
+                table: "Cinemas");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cinemas_Enderecos_enderecoID",
+                table: "Cinemas",
+                column: "enderecoID",
+                principalTable: "Enderecos",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
